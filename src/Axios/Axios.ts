e54e0fb5635ee6d027237517eee81ae1bc616596
TanishqRathi12 <rathitanishq2022@gmail.com>
2024-12-17 04:38:19 +0530

    spell mistake

--- src/Axios/Axios.ts
+++ src/Axios/Axios.ts
@@ -2,7 +2,7 @@ import instanceAxios from 'axios';
 
 
 export const axios = instanceAxios.create({
-    aseURL: 'https://notes-listing-backend.onrender.com/',
+    baseURL: 'https://notes-listing-backend.onrender.com/',
     //baseURL: 'http://localhost:5000',
     headers: {
         'Content-Type': 'application/json',
